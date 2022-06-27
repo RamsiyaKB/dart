c361a9cd622fc84522bb5a2f51aa45c414be930c
RamsiyaKB <108264316+RamsiyaKB@users.noreply.github.com>
2022-06-27 23:41:09 +0530

    Created list .dart

---
+++ List.dart
@@ -0,0 +1,14 @@
+void main() 
+{
+  List<int> integer = [1,2,3,4];
+  print(integer);
+
+  List<String> fruit = ['apple','orange'];
+  print(fruit[1]);
+  
+  List<dynamic> bp = ['jennie','lisa','rose','jisoo'];
+  print(bp);
+  
+}
+
+  
