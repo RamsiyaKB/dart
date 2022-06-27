242a31c4de255143daa8c490f2a316dde7c458bd
RamsiyaKB <108264316+RamsiyaKB@users.noreply.github.com>
2022-06-27 15:47:51 +0530

    Declared double and integer

---
+++ Integer.dart
@@ -0,0 +1,11 @@
+void main() {
+   // declare an integer
+   int num1 = 10;             
+     
+   // declare a double value
+   double num2 = 10.50;  
+
+   // print the values
+   print(num1);
+   print(num2);
+}
