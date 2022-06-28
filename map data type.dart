2d5e2f2fb71a8d4771feb22864928e8270ac4937
RamsiyaKB <108264316+RamsiyaKB@users.noreply.github.com>
2022-06-28 15:11:01 +0530

    Create map data type.dart

---
+++ map data type.dart
@@ -0,0 +1,25 @@
+void main()
+{
+  Map<String,String>student={'a':'ss','b':'dd'};
+  
+  //find the number of key/value pairs using length
+  
+   print(student.length);
+  
+  //check if a Map is empty or not
+  
+  print(student.isEmpty);
+  print(student.isNotEmpty);
+  
+  //get all keys or values with keys & values property.
+  
+  print(student.keys);
+  print(student.values);
+  
+  //get value of specified key in a Map or operator []. 
+  
+  print(student['a']);
+  
+  
+  
+  }
