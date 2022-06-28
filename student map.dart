31b326aaf61431b4221020255b33774a203ff0aa
RamsiyaKB <108264316+RamsiyaKB@users.noreply.github.com>
2022-06-28 11:14:46 +0530

    created a student map
    created a student map with details of Name , 
    Admission No , Total mark .

---
+++ student map.dart
@@ -0,0 +1,9 @@
+// Create a Student map with details of Name , 
+// Admission No , Total mark 
+
+void main() { 
+   Map<String ,dynamic > student = {'name':'kim namjoon','adm no':'777','total mark':'99'};
+   
+  print(student);
+
+}
